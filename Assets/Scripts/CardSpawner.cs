c1d7be085f303fc764ed09a1b4b5c620e8474198
JpBalajiPixcellplay <jpbalaji.pixcellplay@gmail.com>
2023-06-04 19:21:56 +0530

    Card Spawn points spawn changes

--- Assets/Scripts/CardSpawner.cs
+++ Assets/Scripts/CardSpawner.cs
@@ -2,24 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class SpawnPosition
-{
-    public List<Transform> spawnPositionList;
-    public Vector3 spawnHolderPosition;
-}
+
 public class CardSpawner : MonoBehaviour
 {
-    public bool testing;
     public GameObject cardPrefab;
     public Transform cardHolder;
     public int cardsCount;
+    public float spawnPointRandomPrefabValue;
     public List<CardData> cardDataSO = new List<CardData>();
-    public List<Transform> spawnPointsList;
+    public List<Transform> spawnPointsList=new List<Transform>();
     private List<GameObject> selectedCards;
-    private float spawnRandomValue;
-    private List<GameObject> currentCardGameObjects=new List<GameObject>();   
+    private List<GameObject> currentCardGameObjects=new List<GameObject>();
 
+    [Header("RANDOM_CARD_VALIDATION")]
     public List<int> randomValCard4=new List<int>(); 
     public List<int> randomValCard6 = new List<int>();
     public List<int> randomValCard8 = new List<int>();  
@@ -27,20 +22,15 @@ public class CardSpawner : MonoBehaviour
     public List<int> randomValCard12 = new List<int>();
     public List<int> randomValCard16 = new List<int>();
 
+    [Header("SPAWNPOINTS_PREFAB")]
+    public List<GameObject> prefab_Card4;
+    public List<GameObject> prefab_Card6;
+    public List<GameObject> prefab_Card8;  
+    public List<GameObject> prefab_Card10;
+    public List<GameObject> prefab_Card12;
+    public List<GameObject> prefab_Card16;
     public Transform testSpawnArea;
 
-
-
-    public SpawnPosition cards4;
-    public SpawnPosition cards6_0;
-    public SpawnPosition cards6_1;
-    public SpawnPosition cards8_0;
-    public SpawnPosition cards8_1; 
-    public SpawnPosition cards10_0;
-    public SpawnPosition cards10_1;
-    public SpawnPosition cards12_0;
-    public SpawnPosition cards12_1;
-    public SpawnPosition cards16;
     // Start is called before the first frame update
     void Start()
     {
@@ -50,230 +40,64 @@ public class CardSpawner : MonoBehaviour
     [ContextMenu("SpawnCards")]
     void CreateSpawnPoints()
     {
-        if (testing)
-        {
-            spawnRandomValue = -1;
-            DeleteRandomCards();
-        }
-        if(spawnRandomValue == -1)
-        {
-            ChooseCardType();
-            ClearSpawnPoints();
-            float randValue = Random.Range(0, 2);
-            spawnRandomValue = randValue;
-        }
-        else
-        {
-            DeleteRandomCards();
-            DeleteCards();
-        }
+        DeleteRandomCards();
+        ChooseCardType();
+        ChooseSpawnPointPrefab();
 
         switch (cardsCount)
         {
             case 4:
-                cardHolder.position = cards4.spawnHolderPosition;
                 CardMatchValidation(randomValCard4);
-                CardSpawn(cards4.spawnPositionList);
+                CardSpawn(spawnPointsList);
                 break;
             case 6:
                 CardMatchValidation(randomValCard6);
-                if (spawnRandomValue == 0)
-                {
-                    cardHolder.position = cards6_0.spawnHolderPosition;
-                    CardSpawn(cards6_0.spawnPositionList);
-                }
-                else
-                {
-                    cardHolder.position = cards6_1.spawnHolderPosition;
-                    CardSpawn(cards6_1.spawnPositionList);
-                }
+                CardSpawn(spawnPointsList);
                 break;
             case 8:
                 CardMatchValidation(randomValCard8);
-                if (spawnRandomValue == 0)
-                {
-                    cardHolder.position = cards8_0.spawnHolderPosition;
-                    CardSpawn(cards8_0.spawnPositionList);
-                }
-                else
-                {
-                    cardHolder.position = cards8_1.spawnHolderPosition;
-                    CardSpawn(cards8_1.spawnPositionList);
-                }
+                CardSpawn(spawnPointsList);
                 break;
             case 10:
                 CardMatchValidation(randomValCard10);
-                if (spawnRandomValue == 0)
-                {
-                    cardHolder.position = cards10_0.spawnHolderPosition;
-                    CardSpawn(cards10_0.spawnPositionList);
-                }
-                else
-                {
-                    cardHolder.position = cards10_1.spawnHolderPosition;
-                    CardSpawn(cards10_1.spawnPositionList);
-                }
+                CardSpawn(spawnPointsList);
                 break;
             case 12:
                 CardMatchValidation(randomValCard12);
-                if (spawnRandomValue == 0)
-                {
-                    cardHolder.position = cards12_0.spawnHolderPosition;
-                    CardSpawn(cards12_0.spawnPositionList);
-                }
-                else
-                {
-                    cardHolder.position = cards12_1.spawnHolderPosition;
-                    CardSpawn(cards12_1.spawnPositionList);
-                }
+                CardSpawn(spawnPointsList);
                 break;
             case 16:
                 CardMatchValidation(randomValCard16);
-                cardHolder.position = cards16.spawnHolderPosition;
-                CardSpawn(cards16.spawnPositionList);
+                CardSpawn(spawnPointsList);
                 break;
         }
     }
 
-    [ContextMenu("Test Delete")]
-    void DeleteCards()
+    void DeleteCardSpawnPointPrefab()
     {
-        switch (cardsCount)
+        if (cardHolder.childCount > 0)
         {
-            case 4:
-                ClearCards(cards4.spawnPositionList);
-                break;
-            case 6:
-                if (spawnRandomValue == 0)
-                {
-                    ClearCards(cards6_0.spawnPositionList);
-                }
-                else
-                {
-                    ClearCards(cards6_1.spawnPositionList);
-                }
-                break;
-            case 8:
-                if (spawnRandomValue == 0)
-                {
-                    ClearCards(cards8_0.spawnPositionList);
-                }
-                else
-                {
-                    ClearCards(cards8_1.spawnPositionList);
-                }
-                break;
-            case 10:
-                if (spawnRandomValue == 0)
-                {
-                    ClearCards(cards10_0.spawnPositionList);
-                }
-                else
-                {
-                    ClearCards(cards10_1.spawnPositionList);
-                }
-                break;
-            case 12:
-                if (spawnRandomValue == 0)
-                {
-                    ClearCards(cards12_0.spawnPositionList);
-                }
-                else
-                {
-                    ClearCards(cards12_1.spawnPositionList);
-                }
-                break;
-            case 16:
-                ClearCards(cards16.spawnPositionList);
-                break;
+            Destroy(cardHolder.GetChild(0).gameObject);
         }
     }
-    
+
     void DeleteRandomCards()
     {
         randomValCard4.Clear();
         randomValCard6.Clear();
         randomValCard8.Clear();
         randomValCard10.Clear();
-        randomValCard12.Clear(); 
+        randomValCard12.Clear();
         randomValCard16.Clear();
     }
-    void ClearCards(List<Transform> spawnPositions)
+
+    void ClearSpawnPointsList()
     {
-        for (int i = 0; i < spawnPositions.Count; i++)
+        if (spawnPointsList.Count > 0)
         {
-           Destroy(spawnPositions[i].GetChild(0).gameObject);
+            spawnPointsList.Clear();
         }
     }
-    void ClearSpawnPoints()
-    {
-
-        for (int i = 0; i < spawnPointsList.Count; i++)
-        {
-            spawnPointsList[i].gameObject.SetActive(false);
-        }
-        cardHolder.position = Vector3.zero;
-    }
-
-    void CardMatchValidation(List<int> randCardNumberList)
-    {
-        for (int i = 0; i < cardsCount; i++)
-        {
-            if (i < 2)
-            {
-                CardCreation(randCardNumberList[0]);
-            }
-            else if (i >= 2 && i < 4)
-            {
-                CardCreation(randCardNumberList[1]);
-            }
-            else if (i >= 4 && i < 6)
-            {
-                CardCreation(randCardNumberList[2]);
-            }
-            else if (i >= 6 && i < 8)
-            {
-                CardCreation(randCardNumberList[3]);
-            }
-            else if (i >= 8 && i < 10)
-            {
-                CardCreation(randCardNumberList[4]);
-            }
-            else if (i >= 10 && i < 12)
-            {
-                CardCreation(randCardNumberList[5]);
-            }
-        }
-    }
-    void CardCreation( int randomValueList)
-    {
-        var card = Instantiate(cardPrefab, testSpawnArea);
-        card.GetComponent<Card>().cardDataSo = cardDataSO[randomValueList]; 
-        card.GetComponent<Card>().CardSetUp();
-        currentCardGameObjects.Add(card);
-    }  
-    
-    
-    void CardSpawn(List<Transform> spawnPosList)
-    {   
-        for (int i = 0; i < spawnPosList.Count; i++)
-        {
-            float randomId = Random.Range(0,currentCardGameObjects.Count);
-            spawnPosList[i].gameObject.SetActive(true);
-            Instantiate(currentCardGameObjects[(int)randomId], spawnPosList[i]);
-            currentCardGameObjects.RemoveAt((int)randomId);
-        }
-        if (testSpawnArea.childCount > 0)
-        {
-            for (int i = 0; i < testSpawnArea.childCount; i++)
-            {
-                Destroy(testSpawnArea.GetChild(i).gameObject);
-            }
-            currentCardGameObjects.Clear();
-        }
-    }
-
-
     void ChooseCardType()
     {
         switch (cardsCount)
@@ -282,7 +106,7 @@ public class CardSpawner : MonoBehaviour
                 float randomVal4_1 = Random.Range(0, cardDataSO.Count);
                 randomValCard4.Add((int)randomVal4_1);
                 float randomVal4_2 = Random.Range(0, cardDataSO.Count);
-                if(randomVal4_2 == randomVal4_1)
+                if (randomVal4_2 == randomVal4_1)
                 {
                     randomVal4_2 = Random.Range(0, cardDataSO.Count);
                     randomValCard4.Add((int)randomVal4_2);
@@ -353,13 +177,159 @@ public class CardSpawner : MonoBehaviour
                 break;
         }
     }
-    //void CardSpawn()
-    //{
-    //    for (int i = 0; i < cardsCount; i++)
-    //    {
-    //        var card = Instantiate(cardPrefab, cardHolder);
-    //        card.GetComponent<Card>().cardDataSo = cardDataSO; 
-    //        card.GetComponent<Card>().CardSetUp();
-    //    }
-    //}
+
+    void ChooseSpawnPointPrefab()
+    {
+        switch (cardsCount)
+        {
+            case 4:
+                if (spawnPointRandomPrefabValue != -1)
+                {
+                    ClearSpawnPointsList();
+                    DeleteCardSpawnPointPrefab();
+                }
+                else
+                {
+                    spawnPointRandomPrefabValue = Random.Range(0, prefab_Card4.Count);
+                }
+                SpawnPointPrefabSpawn(prefab_Card4);
+                break;
+            case 6:
+                if (spawnPointRandomPrefabValue != -1)
+                {
+                    ClearSpawnPointsList();
+                    DeleteCardSpawnPointPrefab();
+                }
+                else
+                {
+                    spawnPointRandomPrefabValue = Random.Range(0, prefab_Card6.Count);
+                }
+                SpawnPointPrefabSpawn(prefab_Card6);
+                break;
+            case 8:
+                if (spawnPointRandomPrefabValue != -1)
+                {
+                    ClearSpawnPointsList();
+                    DeleteCardSpawnPointPrefab();
+                }
+                else
+                {
+                    spawnPointRandomPrefabValue = Random.Range(0, prefab_Card8.Count);
+                }
+                SpawnPointPrefabSpawn(prefab_Card8);
+                break;
+            case 10:
+                if (spawnPointRandomPrefabValue != -1)
+                {
+                    ClearSpawnPointsList();
+                    DeleteCardSpawnPointPrefab();
+                }
+                else
+                {
+                    spawnPointRandomPrefabValue = Random.Range(0, prefab_Card10.Count);
+                }
+                SpawnPointPrefabSpawn(prefab_Card10);
+                break;
+            case 12:
+                if (spawnPointRandomPrefabValue != -1)
+                {
+                    ClearSpawnPointsList();
+                    DeleteCardSpawnPointPrefab();
+                }
+                else
+                {
+                    spawnPointRandomPrefabValue = Random.Range(0, prefab_Card12.Count);
+                }
+                SpawnPointPrefabSpawn(prefab_Card12);
+                break;
+            case 16:
+                if (spawnPointRandomPrefabValue != -1)
+                {
+                    ClearSpawnPointsList();
+                    DeleteCardSpawnPointPrefab();
+                }
+                else
+                {
+                    spawnPointRandomPrefabValue = Random.Range(0, prefab_Card16.Count);
+                }
+                SpawnPointPrefabSpawn(prefab_Card16);
+                break;
+        }
+
+    }
+
+    void SpawnPointPrefabSpawn(List<GameObject> prefabList)
+    {
+        var spawnPointsPrefab = Instantiate(prefabList[(int)spawnPointRandomPrefabValue], cardHolder);
+        for (int i = 0; i < cardsCount; i++)
+        {
+            spawnPointsList.Add(spawnPointsPrefab.transform.GetChild(i));
+        }
+    }
+
+    void CardMatchValidation(List<int> randCardNumberList)
+    {
+        for (int i = 0; i < cardsCount; i++)
+        {
+            if (i < 2)
+            {
+                CardCreation(randCardNumberList[0]);
+            }
+            else if (i >= 2 && i < 4)
+            {
+                CardCreation(randCardNumberList[1]);
+            }
+            else if (i >= 4 && i < 6)
+            {
+                CardCreation(randCardNumberList[2]);
+            }
+            else if (i >= 6 && i < 8)
+            {
+                CardCreation(randCardNumberList[3]);
+            }
+            else if (i >= 8 && i < 10)
+            {
+                CardCreation(randCardNumberList[4]);
+            }
+            else if (i >= 10 && i < 12)
+            {
+                CardCreation(randCardNumberList[5]);
+            } 
+            else if (i >= 12 && i < 16)
+            {
+                CardCreation(randCardNumberList[5]);
+            }
+        }
+    }
+
+    void CardCreation( int randomValueList)
+    {
+        var card = Instantiate(cardPrefab, testSpawnArea);
+        Card cardScript = card.GetComponent<Card>();
+        cardScript.cardDataSo = cardDataSO[randomValueList];
+        cardScript.CardSetUp();
+        currentCardGameObjects.Add(card);
+    }  
+    
+    void CardSpawn(List<Transform> spawnPosList)
+    {   
+        for (int i = 0; i < spawnPosList.Count; i++)
+        {
+            float randomId = Random.Range(0,currentCardGameObjects.Count);
+            //Debug.Log("RID: " + randomId + "CC: "+ currentCardGameObjects.Count);
+            spawnPosList[i].gameObject.SetActive(true);
+            Instantiate(currentCardGameObjects[(int)randomId], spawnPosList[i]);
+            currentCardGameObjects.RemoveAt((int)randomId);
+        }
+        if (testSpawnArea.childCount > 0)
+        {
+            for (int i = 0; i < testSpawnArea.childCount; i++)
+            {
+                Destroy(testSpawnArea.GetChild(i).gameObject);
+            }
+            currentCardGameObjects.Clear();
+        }
+    }
+
+   
 }
